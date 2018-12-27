17a404147b86826a5051178f27554de29c33bddd
defkhan1 <hasankhan9@gmail.com>
2018-12-27 18:45:49 -0500

    l;dfk

--- app.py
+++ app.py
@@ -15,31 +15,31 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 def getData (ticker, year):
-    datestart = '%d-01-01' %year
-    dateend = '%d-12-31' %year
-    response = requests.get('https://www.quandl.com/api/v3/datatables/WIKI/Prices.json?ticker=%s&date.gte=%s&date.lte=%s&qopts.columns=ticker,date,open,close&api_key=oGPHaajGy6WuHobANi6p' %(ticker,datestart,dateend))
-    return response
+	datestart = '%d-01-01' %year
+	dateend = '%d-12-31' %year
+	response = requests.get('https://www.quandl.com/api/v3/datatables/WIKI/Prices.json?ticker=%s&date.gte=%s&date.lte=%s&qopts.columns=ticker,date,open,close&api_key=oGPHaajGy6WuHobANi6p' %(ticker,datestart,dateend))
+	return response
 
 @app.route('/graph')
 def displayPlot():
 	ticker = 'GOOG'
 	year = 2017
 	r = getData(ticker, year)
-    df = pd.DataFrame(r.json()['datatable']['data'])
-    df.columns = pd.DataFrame(r.json()['datatable']['columns'])['name']
-    df.set_index(pd.DatetimeIndex(df['date']), inplace=True)
-    source = ColumnDataSource(df)
-    p1 = figure(x_axis_type="datetime", title="Quandl WIKI Stock Closing Prices - %d" %year)
-    p1.grid.grid_line_alpha=2.0
-    p1.xaxis.axis_label = 'Date'
-    p1.yaxis.axis_label = 'Price'
+	df = pd.DataFrame(r.json()['datatable']['data'])
+	df.columns = pd.DataFrame(r.json()['datatable']['columns'])['name']
+	df.set_index(pd.DatetimeIndex(df['date']), inplace=True)
+	source = ColumnDataSource(df)
+	p1 = figure(x_axis_type="datetime", title="Quandl WIKI Stock Closing Prices - %d" %year)
+	p1.grid.grid_line_alpha=2.0
+	p1.xaxis.axis_label = 'Date'
+	p1.yaxis.axis_label = 'Price'
 
-    p1.line('date', 'close', color='#0000FF', legend='%s: Closing Price' %ticker, source = source)
-    p1.legend.location = "top_left"
+	p1.line('date', 'close', color='#0000FF', legend='%s: Closing Price' %ticker, source = source)
+	p1.legend.location = "top_left"
 
-    script, div = components(p1)
+	script, div = components(p1)
 
-    return render_template('graph.html', the_div = div, the_script = script)
+	return render_template('graph.html', the_div = div, the_script = script)
 
     #output_file("stocks.html", title="Stock Closing Proces")
 #    script, div = components(p1)
