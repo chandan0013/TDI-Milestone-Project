d841e818001cb50aaed8a0923a15fe34e01f0140
defkhan1 <hasankhan9@gmail.com>
2018-12-28 11:42:57 -0500

    It finally works!

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import requests
 import pandas as pd
-import bokeh
-from bokeh.layouts import column, widgetbox
-from bokeh.plotting import figure, show, output_file
-from bokeh.embed import components
-from bokeh.models.sources import ColumnDataSource
-from bokeh.models.widgets import Button, TextInput
-from bokeh.io import curdoc
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 app = Flask(__name__)
 
@@ -19,22 +14,23 @@ def getData (ticker, year, price):
 
 def processData (ticker, year, price):
 	r = getData(ticker, year, price)
-	if r.json()['datatable']['data']:
-		df = pd.DataFrame(r.json()['datatable']['data'])
+	rjs = r.json()['datatable']['data']
+	if rjs:
+		df = pd.DataFrame(rjs)
 		df.columns = pd.DataFrame(r.json()['datatable']['columns'])['name']
 		df.set_index(pd.DatetimeIndex(df['date']), inplace=True)
 	else:
-		df = None
+		df = None #Return None if ticker or year are invalid
 	return df
 
 def makePlot (df, ticker, year, price):
-	p = figure(x_axis_type="datetime", title="Quandl WIKI Stock Data - %d" %year)
-	p.grid.grid_line_alpha=2.0
-	p.xaxis.axis_label = 'Date'
-	p.yaxis.axis_label = 'Price (USD)'
-	p.line(df.index,df[price], color='#0000FF', legend='%s: %s' %(ticker,price))
-	p.legend.location = "top_left"
-	script, div = components(p)
+	plot = figure(x_axis_type="datetime", title="Quandl WIKI Stock Data - %d" %year)
+	plot.grid.grid_line_alpha=2.0
+	plot.xaxis.axis_label = 'Date'
+	plot.yaxis.axis_label = 'Price (USD)'
+	plot.line(df.index, df[price], color='#0000FF', legend='%s: %s' %(ticker,price))
+	plot.legend.location = "top_left"
+	script, div = components(plot)
 	return script, div
 
 @app.route('/', methods=['GET','POST'])
@@ -43,13 +39,13 @@ def index():
 
 @app.route('/graph', methods=['POST'])
 def graph():
-	ticker, price = request.form['tickerInput'].upper(), request.form['priceInput']
+	ticker, price, year = request.form['tickerInput'].upper(), request.form['priceInput'], request.form['yearInput']
 	
 	#Somewhat sloppy way of doing error checking...
-	if ticker == '' or request.form['yearInput'] == '':
+	if ticker == '' or year == '':
 		df = None
 	else:
-		year = int(request.form['yearInput'])	
+		year = int(year)	
 		df = processData(ticker, year, price)
 
 	if type(df) == pd.DataFrame:
@@ -58,8 +54,7 @@ def graph():
 		
 	else:
 		err = 'Uhoh! Something went wrong. :( Either we do not have data for that ticker/year combo or you entered an invalid ticker and/or year. Please enter a valid ticker and year.'
-		return render_template('index.html', err=err)#redirect(url_for('index.html', err=err))
+		return render_template('index.html', err=err)
 
 if __name__ == '__main__':
 	app.run(port=33507)
-
